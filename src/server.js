ba6adcf78afdbecf308b791a34c14c9e9e0e8f82
Jaro111 <jarokark@protonmail.com>
2026-02-27 22:23:12 +0000

    origin

--- src/server.js
+++ src/server.js
@@ -13,9 +13,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: true,
+    origin: "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
 
